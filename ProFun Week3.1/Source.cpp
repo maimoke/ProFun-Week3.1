b1ad878b469c7635939a186ec335cfea87291cb2
ชญานิน เลียงจินดาถาวร <maimokemm@hotmail.com>
2020-08-28 00:29:39 +0700

    เปลี่ยนในทางที่ดี

--- ProFun Week3.1/Source.cpp
+++ ProFun Week3.1/Source.cpp
@@ -1,9 +1,15 @@
 #include<stdio.h>
 int main()
 {
-	char a;
-	scanf_s("%c", &a);
-	if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u') printf("There is vowel");
-	else printf("There is not vowel");
+	bool vowel = 0;
+	int i;
+	char a[1000];
+	scanf_s("%s", &a,1000);
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u') 
+			vowel =1;
+	}
+	if (vowel == 0) printf("There is no vowel"); else printf("There is vowel");
 	return 0;
 }
